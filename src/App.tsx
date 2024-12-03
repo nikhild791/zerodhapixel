87c7a12989db0b5fdc38bc10126e4a8ac1e1332b
nikhild791 <dubeynikhil791@gmail.com>
2024-12-03 16:54:46 +0530

    colleted all resources

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+   <>
+   <Navbar/>
+   <div className='text-xl underline'>Hi there</div>
+   </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <img src="/src/assets/images" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
